a3b52f74313353aeef22db7776055ed52cf77c2b
Gabor Szarnyas <szarnyasg@gmail.com>
2012-07-24 14:36:40 +0200

    changed node save order

--- src/hu/bme/mit/neo4jmapping/Neo4jTestBed.java
+++ src/hu/bme/mit/neo4jmapping/Neo4jTestBed.java
@@ -20,21 +20,24 @@ public class Neo4jTestBed {
 
 	private String databasePath;
 
-	public Neo4jTestBed(String databasePath, boolean deletePrevious)
-			throws IOException {
+	public Neo4jTestBed(String databasePath, boolean deletePrevious) {
 		this.databasePath = databasePath;
 
-		// deleting test database directory
-		if (new File(databasePath).exists() && deletePrevious) {
-			try {
-				FileUtils.deleteRecursively(new File(databasePath));
-				System.out.println("Database directory deleted.");
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.out.println("Cannot delete database directory.");
-				throw e;
-			}
-		}
+		try {
+			// deleting test database directory
+			if (new File(databasePath).exists() && deletePrevious) {
+				try {
+					FileUtils.deleteRecursively(new File(databasePath));
+					System.out.println("Database directory deleted.");
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.out.println("Cannot delete database directory.");
+					throw e;
+				}
+			}			
+		} catch (Exception e) {
+			// TODO: handle exception
+		} 
 	}
 
 	private GraphDatabaseAPI graphDatabaseAPI = null;
@@ -67,32 +70,37 @@ public class Neo4jTestBed {
 		Transaction tx = graphDatabaseAPI.beginTx();
 
 		GraphRepository<MyNode> graphRepository = template.repositoryFor(MyNode.class);
-
+		
 		MyNode m0 = new MyNode("Node 0", 0);
 		m0.nodes = new HashSet<MyNode>();
-
-		int linkedListLength = 800;
-		int starLeafCount = 500;
-
-		// linked list
-		if (linkedListLength > 0) {
-			MyNode lastMovie = m0;
-			for (int i = 1; i < linkedListLength; i++) {
-				MyNode m = new MyNode("Linked list " + i, i);
-				lastMovie.nextNode = m;
-				lastMovie = m;
-			}
-		}
+	    graphRepository.save(m0);
+		
+		int linkedListLength = 5;
+		//int starLeafCount = 1000;
 
 		// star graph
+		/*
 		if (starLeafCount > 0) {
 			for (int i = 1; i < starLeafCount; i++) {
 				MyNode m = new MyNode("Star " + i, linkedListLength + i);
 				m0.nodes.add(m);
+				graphRepository.save(m);
+				graphRepository.save(m0);
 			}
-		}
-
-		graphRepository.save(m0);
+		}*/
+		
+	    // linked list
+	    if (linkedListLength > 0) {
+	        MyNode lastNode = m0;
+	        for (int i = 1; i < linkedListLength; i++) {
+	            MyNode m = new MyNode("Linked list " + i, i);
+	            lastNode.nextNode = m;
+	            graphRepository.save(m);
+	            graphRepository.save(lastNode);
+	            lastNode = m;
+	        }
+	    }
+	    //graphRepository.save(m0);
 
 		tx.success();
 		tx.finish();
